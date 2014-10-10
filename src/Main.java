64b9591ea6f4c64d6e263e81377a4aad7b42511a
Stretch Projects <StretchProjects@gmail.com>
2014-10-10 20:15:58 +0100

    Files to accompany Youtube series

---
+++ src/Main.java
@@ -0,0 +1,22 @@
+/**
+ * Author   : Stretch Projects
+ * Series   : Introduction to Java
+ * Topic    : Refactoring
+ * Lecture  : http://youtu.be/70YIc7U8FLk
+ * Tutorial : http://youtu.be/k3y35D-0KEo
+ * License  : None.  Do with it as you wish! :)
+ */
+
+public class Main {
+
+	public static void main(String[] args) {	
+		
+		int[] views = {6, 7};
+		
+		int totalViews = views[0] + views[1];
+		
+		System.out.println("Total views: " + 
+				totalViews);
+	}
+
+}
